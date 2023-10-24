af0dba03cc91c82e7c30525d8264ae2d8eff9427
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 12:30:55 -0400

    Add proudct form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { CreateAccountFormComponent } from './create-account-form/create-account-form.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    CreateAccountFormComponent
+    CreateAccountFormComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
