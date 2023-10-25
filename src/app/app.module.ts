441dcd9dbe59799398db52ca4decfa6c49668497
avrubio <ariadna.vanegas@gmail.com>
2023-10-25 15:57:56 -0400

    app.module.ts updated and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { CreateAccountFormComponent } from './create-account-form/create-account-form.component';
+
 import { AddProductComponent } from './add-product/add-product.component';
+import { AppComponent } from './app.component';
+import {
+  CreateAccountFormComponent,
+} from './create-account-form/create-account-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
     CreateAccountFormComponent,
-    AddProductComponent
+    AddProductComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, NgbModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
