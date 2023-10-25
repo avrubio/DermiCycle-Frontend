24e4744dbcecc23b70124bb4cf38a9f4413991f4
avrubio <ariadna.vanegas@gmail.com>
2023-10-25 16:45:24 -0400

    added UserService in create-account-form

--- src/app/create-account-form/create-account-form.component.ts
+++ src/app/create-account-form/create-account-form.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-create-account-form',
   templateUrl: './create-account-form.component.html',
-  styleUrls: ['./create-account-form.component.css']
+  styleUrls: ['./create-account-form.component.css'],
 })
-export class CreateAccountFormComponent {
+export class CreateAccountFormComponent implements OnInit {
+  user = {
+    firstName: '',
+    emailAddress: '',
+    password: '',
+    skinCycle: '',
+    skinType: '',
+  };
+  jwt: string = '';
 
+  constructor(private signupService: UserService) {}
+
+  signUp(): void {
+    this.signupService
+      .signUp(
+        this.user.firstName,
+        this.user.emailAddress,
+        this.user.password,
+        this.user.skinCycle,
+        this.user.skinType
+      )
+      .subscribe(
+        (response) => {
+          console.log('Account signup successful', response);
+        },
+        (error) => {
+          console.error('Account signup failed', error);
+          // Handle login errors here (e.g., display an error message)
+        }
+      );
+  }
+
+  ngOnInit(): void {}
 }
