30b71c31a6eb72dcf361316342b99f8fc72dd2b6
avrubio <ariadna.vanegas@gmail.com>
2023-10-25 15:57:20 -0400

    userService updated now working

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class UserService {
+  private apiUrl = 'http://localhost:0317/auth/users/login/';
+
   constructor(private http: HttpClient) {}
 
-  login(userData: any): Observable<any> {
-    // Make sure to specify the response type you expect
-    const apiUrl = 'http://localhost:0317/auth/users/login';
-    return this.http.post(apiUrl, userData);
+  login(emailAddress: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { emailAddress, password };
+
+    return this.http.post(this.apiUrl, body, { headers });
   }
 }
