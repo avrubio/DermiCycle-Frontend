63e7109bfd53979b92a65a12179698ed7a94e6b8
avrubio <ariadna.vanegas@gmail.com>
2023-10-26 10:13:59 -0400

    fixed the log dermicycle description component position

---
+++ src/app/landing/dermicycle-description/dermicycle-description.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dermicycle-description',
+  templateUrl: './dermicycle-description.component.html',
+  styleUrls: ['./dermicycle-description.component.css']
+})
+export class DermicycleDescriptionComponent {
+
+}
